1bd93e95bcfbf33271026b7528d175a02b8d8faa
Irfansk007 <irfanshaikh2852000@gmail.com>
2022-11-08 22:59:02 +0530

    first commit

---
+++ js/1.js
@@ -0,0 +1,21 @@
+// function add(){
+//     if(arguments.length==0){
+//         console.log("no pareters has been passed");
+//     }
+//     else{
+//         let sum=0;
+//         for (let i = 0; i < arguments.length; i++) {
+//             sum = sum + arguments[i];
+            
+//         }
+//         console.log(sum);
+//     }
+// }
+
+// add(4,6,7);
+
+// let k={
+//     firstname : "irfan",
+//     lastname : "shaikh"
+// }
+// console.log(k);
